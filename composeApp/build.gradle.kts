be08b1b2af043fc2014c634f3c68a2d1c7182489
Shubham Kumar Gupta <gptshubham595@gmail.com>
2025-09-11 19:59:19 +0530

    Add Room compiler dependencies for KSP

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -120,6 +120,10 @@ android {
 
 dependencies {
     debugImplementation(compose.uiTooling)
+    add("kspAndroid", libs.androidx.room.compiler)
+    add("kspIosSimulatorArm64", libs.androidx.room.compiler)
+    add("kspIosX64", libs.androidx.room.compiler)
+    add("kspIosArm64", libs.androidx.room.compiler)
 }
 
 compose.desktop {
