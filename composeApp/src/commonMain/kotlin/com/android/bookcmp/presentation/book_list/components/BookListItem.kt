fad50fb588711e37eb3d14da2cbbc180454c28ff
Shubham Kumar Gupta <gptshubham595@gmail.com>
2025-09-16 13:35:08 +0530

    added book item

--- composeApp/src/commonMain/kotlin/com/android/bookcmp/presentation/book_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/com/android/bookcmp/presentation/book_list/components/BookListItem.kt
@@ -2,16 +2,25 @@ package com.android.bookcmp.presentation.book_list.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,13 +30,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
 import bookcmp.composeapp.generated.resources.Res
 import bookcmp.composeapp.generated.resources.book_error
 import coil3.compose.rememberAsyncImagePainter
 import com.android.bookcmp.core.domain.Book
 import com.android.bookcmp.presentation.ui.Dimens
 import com.android.bookcmp.presentation.ui.LightBlue
+import com.android.bookcmp.presentation.ui.SandYellow
 import org.jetbrains.compose.resources.painterResource
+import kotlin.math.round
 
 @Composable
 fun BookListItem(
@@ -44,11 +56,13 @@ fun BookListItem(
             modifier = Modifier
                 .padding(Dimens.dp_16)
                 .fillMaxWidth()
-                .height(IntrinsicSize.Min)
+                .height(IntrinsicSize.Min),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Box(
                 modifier = Modifier
-                    .height(Dimens.dp_100),
+                    .height(Dimens.dp_100)
+                    .padding(end = Dimens.dp_16),
                 contentAlignment = Alignment.Center
             ) {
                 var imageLoadResult by remember { mutableStateOf<Result<Painter>?>(null) }
@@ -79,8 +93,52 @@ fun BookListItem(
                         )
                     )
                 }
+            }
+            Column(
+                modifier = Modifier
+                    .weight(1F),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = book.title,
+                    style = MaterialTheme.typography.titleMedium,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                book.authors?.firstOrNull()?.let { authorName ->
+                    Text(
+                        text = authorName,
+                        style = MaterialTheme.typography.bodyMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                }
+                book.averageRating?.let { rating ->
+                    Row(
+                        modifier = Modifier,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "${round((rating * 10) / 10.0)}",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                        Icon(
+                            imageVector = Icons.Default.Star,
+                            contentDescription = "Star Rating",
+                            tint = SandYellow,
+                        )
+                    }
+
+                }
 
             }
+
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                contentDescription = "Right Arrow",
+                modifier = Modifier
+                    .size(Dimens.dp_36)
+            )
 
         }
     }
